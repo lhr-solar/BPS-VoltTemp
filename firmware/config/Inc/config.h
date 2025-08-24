4b46ab1e16cdddaf5040fcea75c146349a4260fa
Lakshay Gupta <lakshay45gupta@gmail.com>
2025-08-23 20:10:20 -0500

    playing w/ github actions

--- firmware/config/Inc/config.h
+++ firmware/config/Inc/config.h
@@ -14,7 +14,7 @@
 // VoltTemp configurations
 // Each VoltTemp configuration defines which battery modules (0 indexed) are connected to that VoltTemp board
 // (VoltTemp ID, {Battery Modules connected}, CAN ID)
-#define VOLTTEMP_CONFIGS       \
+#define VOLTTEMP_CONFIGS \
     VOLTTEMP_CONFIG(VOLTTEMP_0, {0, 1, 2, 3}, 0x01) \
     VOLTTEMP_CONFIG(VOLTTEMP_1, {4, 5, 6, 7}, 0x02) \
     VOLTTEMP_CONFIG(VOLTTEMP_2, {8, 9, 10, 11}, 0x03) \
@@ -22,4 +22,5 @@
     VOLTTEMP_CONFIG(VOLTTEMP_4, {16, 17, 18, 19}, 0x05) \
     VOLTTEMP_CONFIG(VOLTTEMP_5, {20, 21, 22, 23}, 0x06) \
     VOLTTEMP_CONFIG(VOLTTEMP_6, {24, 25, 26, 27}, 0x07) \
-    VOLTTEMP_CONFIG(VOLTTEMP_7, {28, 29, 30, 31}, 0x08)
+    VOLTTEMP_CONFIG(VOLTTEMP_7, {28, 29, 30, 31}, 0x08) 
+    
